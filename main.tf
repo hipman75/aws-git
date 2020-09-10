338a551e31ad82c8145b991ce1895387083af529
hipman75 <hiepmarcusbui@gmail.com>
2020-09-10 15:35:38 -0500

    added vpc and subnet

--- main.tf
+++ main.tf
@@ -2,10 +2,24 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "my-terraform-ec2" {
-  ami = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "my-ec2" {
+  ami = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
   tags = {
-    "key" = "EC2 terraform"
+    "Name" = "EC2 terraform"
+  }
+}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "my-subnet" {
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "terraform-subnet"
   }
 }
