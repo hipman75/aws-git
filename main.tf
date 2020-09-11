eeeb3f95bb6372031a35e1b2b641faf10297b88b
hipman75 <hiepmarcusbui@gmail.com>
2020-09-11 16:21:42 -0500

    corrected route table

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "my-rt" {
   vpc_id = aws_vpc.my-vpc.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my-igw.id
   }
 
